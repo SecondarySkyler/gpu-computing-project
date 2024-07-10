e756ef5f697fe3ab52f313cb1483edfb3a47e893
Cristian <skylerhunter6@gmail.com>
2024-07-10 15:39:03 +0200

    csr conversion

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,10 +1,13 @@
 #include "../include/utilities.hpp"
+#include "../include/matio.h"
 
-void readMatrixCOO(int* &rows, int* &cols, float* &nnz) {
-    std::ifstream file("./test_matrices/494_bus.mtx");
+COO readMatrixCOO(std::string filename) {
+    COO result;
+
+    std::string filePath = "./test_matrices/" + filename + ".mtx";
+    std::ifstream file(filePath);
     if (!file.is_open()) {
         std::cerr << "Error opening file" << std::endl;
-        return;
     }
 
     // ignore comments
@@ -14,24 +17,89 @@ void readMatrixCOO(int* &rows, int* &cols, float* &nnz) {
     // read the matrix size and number of non-zero elements
     int numRows, numCols, numNnz;
     file >> numRows >> numCols >> numNnz;
-
-    std::cout << "Matrix size: " << numRows << "x" << numCols << std::endl;
-    std::cout << "Number of non-zero elements: " << numNnz << std::endl;
+    result.totalRows = numRows;
+    result.totalCols = numCols;
+    result.totalNnz = numNnz;
 
     // initialize the matrix
-    rows = new int[numNnz];
-    cols = new int[numNnz];
-    nnz = new float[numNnz];
+    result.rows = new int[numNnz];
+    result.cols = new int[numNnz];
+    result.values = new float[numNnz];
 
     // read the matrix elements
     for (int line = 0; line < numNnz; line++) {
         float value;
         int currentRow, currentCol;
         file >> currentRow >> currentCol >> value;
-        rows[line] = currentRow;
-        cols[line] = currentCol;
-        nnz[line] = value;
+        result.rows[line] = currentRow;
+        result.cols[line] = currentCol;
+        result.values[line] = value;
     }
+
+    file.close();
+    return result;
+}
+
+int findIndex(float* array, int size, int value) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void mtxToCsr(std::string filename, int &m, int &n, int &nnz,
+    int *&csrRowPtr, int *&csrColIdx, float* &csrVal) {
+    
+    std::string filePath = "./test_matrices/" + filename + ".mtx";
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+    }
+
+    // ignore comments
+    while (file.peek() == '%')
+        file.ignore(2048, '\n');
+
+    // read the matrix size and number of non-zero elements
+    file >> m >> n >> nnz;
+
+    // initialize row ptrs, column indices and values
+    csrRowPtr = new int[m + 1];
+    csrColIdx = new int[nnz];
+    csrVal = new float[nnz];
+    int* auxRowIdx = new int[nnz];
+
+
+    // read the matrix elements, for the moment we ignore the row pointers
+    for (int line = 0; line < nnz; line++) {
+        float value;
+        int currentRow, currentCol;
+        file >> currentRow >> currentCol >> value;
+        auxRowIdx[line] = currentRow;
+        csrColIdx[line] = currentCol;
+        csrVal[line] = value;
+    }
+
+    // create dense matrix
+    float* denseMatrix = (float*) calloc(m * n, sizeof(float));
+
+    // fill dense matrix
+    for (int i = 0; i < nnz; i++) {
+        denseMatrix[auxRowIdx[i] * n + csrColIdx[i]] = csrVal[i];
+    }
+
+    // scan dense matrix to fill csrRowPtr
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (denseMatrix[i * n + j] != 0) {
+                csrRowPtr[i] = findIndex(csrVal, nnz, denseMatrix[i * n + j]);
+                break;
+            }
+        }
+    }
+    csrRowPtr[m] = nnz;
 
     file.close();
 }
