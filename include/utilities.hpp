a361fa11b93ae7c79eee8f5737de291b4e6369fd
Cristian <skylerhunter6@gmail.com>
2024-07-10 15:38:38 +0200

    implemented csr conversion from mtx

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -1,12 +1,42 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+struct COO {
+    int totalRows;
+    int totalCols;
+    int totalNnz;
+    int* rows;
+    int* cols;
+    float* values;
+};
+
 
 /**
  * Read a matrix in COO format from a file
+ * @param filename the name of the .mtx file containing the matrix description
  * 
- * @param rows the array that will store the row indices
- * @param cols the array that will store the column indices
- * @param nnz the array that will store the non-zero elements
+ * @return COO struct containing the matrix in COO format
  */
-void readMatrixCOO(int* &rows, int* &cols, float* &nnz);
+COO readMatrixCOO(std::string filename);
+
+/**
+ * Read a matrix in CSR format from a file
+ * @param filename the name of the .mtx file containing the matrix description
+ * @param m the number of rows of the matrix
+ * @param n the number of columns of the matrix
+ * @param nnz the number of non-zero elements of the matrix
+ * @param csrRowPtr the row pointer array of the matrix
+ * @param csrColIdx the column index array of the matrix
+ * @param csrVal the value array of the matrix
+ */
+void mtxToCsr(
+    std::string filename,
+    int &m,
+    int &n,
+    int &nnz,
+    int *&csrRowPtr,
+    int *&csrColIdx,
+    float *&csrVal
+);
