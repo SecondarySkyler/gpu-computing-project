6e091143b3ae3cc86e1e72334d99e6a7fe5a2e42
Cristian <skylerhunter6@gmail.com>
2024-08-09 10:35:10 +0200

    refactor: using headers to improve code readability

---
+++ include/transpose_coo.cuh
@@ -0,0 +1,9 @@
+#ifndef _TRANSPOSECOO_H_
+#define _TRANSPOSECOO_H_
+
+#include <cuda_runtime.h>
+#include <string>
+
+void transpose_COO(std::string fileName);
+
+#endif
---
+++ include/transpose_csr.cuh
@@ -0,0 +1,8 @@
+#ifndef _TRANSPOSECSR_H_
+#define _TRANSPOSECSR_H_
+
+#include <cuda_runtime.h>
+#include <string>
+
+void transpose_CSR(std::string fileName);
+#endif
